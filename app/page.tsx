81eb3327e27c4a823a0b285feba321eb7867fd31
Veena V <veenavijayakumar4@gmail.com>
2025-06-18 20:23:52 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-'use client'
+'use client';
 
 import Image from "next/image";
-import Link from "next/link"; // Add this at top
+import Link from "next/link";
 
 export default function Home() {
   return (
