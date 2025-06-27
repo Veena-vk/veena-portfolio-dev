2e5148f80c1770f8c768c8a43cb277c6bcf65e4f
Veena V <veenavijayakumar4@gmail.com>
2025-06-27 21:12:18 +0530

    Update CertificatesCard.tsx

--- app/sun/components/CertificatesCard.tsx
+++ app/sun/components/CertificatesCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { certificates } from "./data/certificates";
+import { certificates } from "../data/certificates";
 
 export default function CertificatesCard() {
   return (
