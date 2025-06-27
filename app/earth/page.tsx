e024858c6d92d20fc063078a6618b112ce1c3a23
Veena V <veenavijayakumar4@gmail.com>
2025-06-27 19:26:00 +0530

    Update page.tsx

--- app/earth/page.tsx
+++ app/earth/page.tsx
@@ -7,11 +7,14 @@ import CrackpotSection from "./components/CrackpotSection";
 export default function EarthPage() {
   return (
     <main className="min-h-screen bg-[#F4E8D2] text-[#59200F] px-6 py-12 space-y-16">
-      <h1 className="text-4xl md:text-5xl font-extrabold text-[#59200F] text-center mb-1">
-    	The Earth Section</h1>
-      <p className="text-md md:text-lg text-center italic text-[#8F7C60] mb-8 max-w-2xl mx-auto">
-        A space for evolution — of logic, life, and the lessons between.
-      </p>
+      <div className="text-center mb-6">
+  	<h1 className="text-4xl md:text-5xl font-extrabold text-[#59200F]">
+    	The Earth Section
+  	</h1>
+  	<p className="text-md md:text-lg italic text-[#8F7C60] mt-1">
+    	A space for evolution — of logic, life, and the lessons between.
+  	</p>
+	</div>
       <ThoughtTrailsSection />
       <OuchSection />
       <CrackpotSection />
