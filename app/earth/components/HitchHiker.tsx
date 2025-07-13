73e7455cdcb3701ff8929781ccc133e53a54d66f
Veena V <veenavijayakumar4@gmail.com>
2025-07-13 12:14:51 +0530

    Update HitchHiker.tsx

--- app/earth/components/HitchHiker.tsx
+++ app/earth/components/HitchHiker.tsx
@@ -5,7 +5,12 @@ const HitchHiker = () => {
   return (
    <Section icon="" title="Hitch Hiker's Guide to Design" 
         subtitle="A layered walk through the design universe — from electrons to ethics.">
-      <iframe src="https://habitual-dracorex-368.notion.site/ebd/22f68e6111a780d88a0bfe075d8450da" width="100%" height="600" frameborder="0" allowfullscreen />
+      <iframe
+  src="https://habitual-dracorex-368.notion.site/ebd/22f68e6111a780d88a0bfe075d8450da"
+  style={{ width: "100%", height: "80vh", border: "none" }}
+  allowFullScreen
+/>
+
    </Section>
   );
 };
